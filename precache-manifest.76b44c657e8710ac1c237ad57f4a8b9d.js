8d38e60bd9ccdea5b7d89614845f1f5cfbf86fac
Amirreza Hemmati <dev.amirhemmati1382@gmail.com>
2020-03-11 16:58:40 -0700

    Updates

---
+++ precache-manifest.76b44c657e8710ac1c237ad57f4a8b9d.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408d72daf5b0deb2c9c4a2073b668528",
+    "url": "/pizzawifi/index.html"
+  },
+  {
+    "revision": "1568ef4d93e3c5203336",
+    "url": "/pizzawifi/static/css/2.03843819.chunk.css"
+  },
+  {
+    "revision": "07d2cc269972de9d09fb",
+    "url": "/pizzawifi/static/css/main.e3e26bfb.chunk.css"
+  },
+  {
+    "revision": "1568ef4d93e3c5203336",
+    "url": "/pizzawifi/static/js/2.b88e4afb.chunk.js"
+  },
+  {
+    "revision": "07d2cc269972de9d09fb",
+    "url": "/pizzawifi/static/js/main.580c7ac8.chunk.js"
+  },
+  {
+    "revision": "0df9c17b29f0aefcfb97",
+    "url": "/pizzawifi/static/js/runtime-main.971a41df.js"
+  },
+  {
+    "revision": "7cf60289589a87f5b7aa62988a11a2da",
+    "url": "/pizzawifi/static/media/149777810989966.7cf60289.png"
+  },
+  {
+    "revision": "6c207bf07abfdaaaf13e9c191e49e32f",
+    "url": "/pizzawifi/static/media/149777820085199.6c207bf0.png"
+  },
+  {
+    "revision": "19a719f72b2ae84aa712da728c2a6e1c",
+    "url": "/pizzawifi/static/media/149779651117863.19a719f7.png"
+  },
+  {
+    "revision": "fb7be4ffcdd2bfd0ec3e0000f338f463",
+    "url": "/pizzawifi/static/media/150123256333789.fb7be4ff.png"
+  },
+  {
+    "revision": "6352b793a9058c7a12ca8c79ef4aac7c",
+    "url": "/pizzawifi/static/media/150123290550163.6352b793.png"
+  },
+  {
+    "revision": "606c3bb124ab6121b368c81b01dba928",
+    "url": "/pizzawifi/static/media/150123298736745.606c3bb1.png"
+  },
+  {
+    "revision": "0f0bcd6af4ede96e1691cf38f9ca2f70",
+    "url": "/pizzawifi/static/media/150175428143654.0f0bcd6a.png"
+  },
+  {
+    "revision": "671ded957b3533739f2ef6e5db6eda4e",
+    "url": "/pizzawifi/static/media/Vazir-Black.671ded95.ttf"
+  },
+  {
+    "revision": "905f660c0e7871464d5ed6ea44b84d5c",
+    "url": "/pizzawifi/static/media/Vazir-Black.905f660c.woff"
+  },
+  {
+    "revision": "c2b6ae5b87c97490e28d883b023887ff",
+    "url": "/pizzawifi/static/media/Vazir-Black.c2b6ae5b.eot"
+  },
+  {
+    "revision": "f20a8cce519599799352c601cdea3e54",
+    "url": "/pizzawifi/static/media/Vazir-Black.f20a8cce.woff2"
+  },
+  {
+    "revision": "32a9365c444760242e313a6c0ee5e76a",
+    "url": "/pizzawifi/static/media/Vazir-Bold.32a9365c.woff"
+  },
+  {
+    "revision": "568d89450f813dfc78bd1d0fa948f573",
+    "url": "/pizzawifi/static/media/Vazir-Bold.568d8945.eot"
+  },
+  {
+    "revision": "792a144ff1ad9229e395f84859f84bad",
+    "url": "/pizzawifi/static/media/Vazir-Bold.792a144f.woff2"
+  },
+  {
+    "revision": "e1fa9df04f68b6c11ba73b2cb2c32111",
+    "url": "/pizzawifi/static/media/Vazir-Bold.e1fa9df0.ttf"
+  },
+  {
+    "revision": "02b7e98e3eb2ca491af991581f819368",
+    "url": "/pizzawifi/static/media/Vazir-Light.02b7e98e.woff"
+  },
+  {
+    "revision": "5cc720f109d19bbfc27c52cab0cdc635",
+    "url": "/pizzawifi/static/media/Vazir-Light.5cc720f1.woff2"
+  },
+  {
+    "revision": "6ad416bea23909e93cd6d760aeadeee8",
+    "url": "/pizzawifi/static/media/Vazir-Light.6ad416be.eot"
+  },
+  {
+    "revision": "c0ea13ae845b9c68ddbc17e0191c1abd",
+    "url": "/pizzawifi/static/media/Vazir-Light.c0ea13ae.ttf"
+  },
+  {
+    "revision": "1751192b3cdc70476cb4723627184a78",
+    "url": "/pizzawifi/static/media/Vazir-Medium.1751192b.eot"
+  },
+  {
+    "revision": "ae945af32c3067eb7963d6552d0822e1",
+    "url": "/pizzawifi/static/media/Vazir-Medium.ae945af3.woff2"
+  },
+  {
+    "revision": "d308ce12c35400f4b93472e37eb6736f",
+    "url": "/pizzawifi/static/media/Vazir-Medium.d308ce12.woff"
+  },
+  {
+    "revision": "f8b9455d037d2372295665f61a9b3cf6",
+    "url": "/pizzawifi/static/media/Vazir-Medium.f8b9455d.ttf"
+  },
+  {
+    "revision": "36746dd25d76e268a09ad61173fdb063",
+    "url": "/pizzawifi/static/media/Vazir-Thin.36746dd2.ttf"
+  },
+  {
+    "revision": "83377932706d778888deeb2cf2ed4720",
+    "url": "/pizzawifi/static/media/Vazir-Thin.83377932.woff2"
+  },
+  {
+    "revision": "ae7b6599bcaf624f176d4dbcbe0ce648",
+    "url": "/pizzawifi/static/media/Vazir-Thin.ae7b6599.woff"
+  },
+  {
+    "revision": "c07359b8132ffae1df3b9c89f9e35671",
+    "url": "/pizzawifi/static/media/Vazir-Thin.c07359b8.eot"
+  },
+  {
+    "revision": "63dc8aed0230d5693186695dff5fa598",
+    "url": "/pizzawifi/static/media/Vazir.63dc8aed.ttf"
+  },
+  {
+    "revision": "78fb045cc70b332ad0eca7f78031acf3",
+    "url": "/pizzawifi/static/media/Vazir.78fb045c.woff"
+  },
+  {
+    "revision": "c97b654993f7c62053b0d2b6505b60f2",
+    "url": "/pizzawifi/static/media/Vazir.c97b6549.woff2"
+  },
+  {
+    "revision": "e5b0de94c692df249d1903e3f4412289",
+    "url": "/pizzawifi/static/media/Vazir.e5b0de94.eot"
+  },
+  {
+    "revision": "60888b322019429caa0d3d23f642b4e0",
+    "url": "/pizzawifi/static/media/about.60888b32.jpg"
+  },
+  {
+    "revision": "0e643e7b99f9736287a1d8dcf6cca619",
+    "url": "/pizzawifi/static/media/banner1.0e643e7b.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pizzawifi/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pizzawifi/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pizzawifi/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pizzawifi/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pizzawifi/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a7308ae5a0326af1fa330a11a18414f8",
+    "url": "/pizzawifi/static/media/footer-logo.a7308ae5.png"
+  },
+  {
+    "revision": "0100131f753b1eb4507da419f2c74530",
+    "url": "/pizzawifi/static/media/logo-header.0100131f.png"
+  }
+]);
